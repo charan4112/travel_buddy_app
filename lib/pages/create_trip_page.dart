3759d41e93dce9e1fc93544360258540326f9841
sarvanix <ltirumalasetti1@student.gsu.edu>
2025-04-29 22:14:56 -0400

    Implemented Dashboard with BottomNavigationBar and connected Trips, Explore, Profile pages

---
+++ lib/pages/create_trip_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CreateTripPage extends StatelessWidget {
+  const CreateTripPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("This is the Create Trip page"),
+    );
+  }
+}
--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,16 +1,67 @@
 import 'package:flutter/material.dart';
+import 'create_trip_page.dart';
+import 'nearby_attractions_page.dart';
+import 'profile_page.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = const [
+    CreateTripPage(),         // Trips tab
+    NearbyAttractionsPage(),  // Explore tab
+    ProfilePage(),            // Profile tab
+  ];
+
+  final List<String> _titles = [
+    "My Trips",
+    "Explore",
+    "Profile",
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Dashboard")),
-      body: const Center(
-        child: Text("Welcome to the Travel Buddy Dashboard!"),
+      appBar: AppBar(
+        title: Text(_titles[_selectedIndex]),
+        centerTitle: true,
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.travel_explore),
+            label: 'Trips',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
 }
-cherry
